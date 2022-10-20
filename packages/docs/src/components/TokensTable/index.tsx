8748f3c630073849b93a9a2f85683374d3afc961
Dana Rocha <hello@danarocha.com>
2022-10-20 13:47:29 +0300

    :art: (Tokens) document all tokens in storybook

--- packages/docs/src/components/TokensTable/index.tsx
+++ packages/docs/src/components/TokensTable/index.tsx
@@ -2,21 +2,22 @@ import './styles.css';
 
 type TokensTableProps = {
   tokens: Record<string, string>;
-  isSpacing?: boolean;
+  type?: 'spacing' | 'shadow' | 'general' | 'radii';
   unity?: 'px' | 'rem' | 'none';
 };
 
 export function TokensTable({
   tokens,
   unity = 'rem',
-  isSpacing,
+  type = 'general',
 }: TokensTableProps) {
   return (
     <table className="tokens-table">
       <thead>
         <tr>
           <th>Name</th>
-          {isSpacing && <th></th>}
+          {type === 'spacing' && <th></th>}
+          {type === 'radii' && <th></th>}
           <th>{unity !== 'none' ? 'Pixel' : 'Value'}</th>
           {unity !== 'none' && <th>Rem</th>}
         </tr>
@@ -27,7 +28,8 @@ export function TokensTable({
           return (
             <tr key={key}>
               <td>{key}</td>
-              {isSpacing && (
+
+              {type === 'spacing' && (
                 <td>
                   <div
                     style={{
@@ -38,7 +40,35 @@ export function TokensTable({
                   ></div>
                 </td>
               )}
+
+              {/* {type === 'shadow' && (
+                <td>
+                  <div
+                    style={{
+                      backgroundColor: 'white',
+                      height: 24,
+                      width: value,
+                      boxShadow: value,
+                    }}
+                  ></div>
+                </td>
+              )} */}
+
+              {type === 'radii' && (
+                <td>
+                  <div
+                    style={{
+                      backgroundColor: 'white',
+                      height: 32,
+                      width: key !== 'circle' ? 64 : 32,
+                      borderRadius: value,
+                    }}
+                  ></div>
+                </td>
+              )}
+
               <td>{value}</td>
+
               {unity !== 'none' && (
                 <td>
                   {unity !== 'rem'
